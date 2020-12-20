333970bff5a97b9b2d84d002717ca5d75b78a2ed
hhutku <hhutku.wa@gmail.com>
2020-12-19 22:13:20 -0800

    get function for  /api/notes added

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ var fs = require("fs");
 var PORT = process.env.PORT || 3000;
 
 
+app.get("/api/notes", (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      data = JSON.parse(data);
+      res.send(data);
+    });
+  
+  })
+
+
+  
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
