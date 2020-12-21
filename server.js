11c4e4c12f2cc0fc58de7b83556e7df9514586e0
hhutku <hhutku.wa@gmail.com>
2020-12-20 17:04:39 -0800

    delete /api/notes added

--- server.js
+++ server.js
@@ -24,37 +24,50 @@ function idGenerate(time) {
 
 
 app.get("/api/notes", (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      data = JSON.parse(data);
-      res.send(data);
-    });
-  
-  })
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    res.send(data);
+  });
 
-  app.post("/api/notes", (req, res) => {
-    var time = moment().format();
-    req.body.id = idGenerate(time);
-  
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      data = JSON.parse(data);
-      data.push(req.body);
-  
-      writeFile("./db/db.json", JSON.stringify(data));
-    });
-  
-    res.json(req.body);
-  })
+})
 
-  
+app.post("/api/notes", (req, res) => {
+  var time = moment().format();
+  req.body.id = idGenerate(time);
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    data.push(req.body);
+
+    writeFile("./db/db.json", JSON.stringify(data));
+  });
+
+  res.json(req.body);
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+
+  const deletedId = req.params.id
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    targetNote = data.filter(({ id }) => id != deletedId);
+
+    writeFile("./db/db.json", JSON.stringify(targetNote));
+  });
+
+  res.send("id no: " + deletedId + " deleted");
+
+})
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
